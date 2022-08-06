befb4bc5a4792550f05dcbea520fec27a666f14e
CesarFGuerra <cesarfrancaguerra@gmail.com>
2022-08-06 09:49:18 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -20,7 +20,7 @@ public class OrderItem implements Serializable {
 	private OrderItemPK id = new OrderItemPK();
 
 	private Integer quantity;
-	private Double Price;
+	private Double price;
 	
 	public OrderItem() {
 	}
@@ -30,7 +30,7 @@ public class OrderItem implements Serializable {
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
-		Price = price;
+		this.price = price;
 	}
 	
 	@JsonIgnore
@@ -59,11 +59,15 @@ public class OrderItem implements Serializable {
 	}
 
 	public Double getPrice() {
-		return Price;
+		return price;
 	}
 
 	public void setPrice(Double price) {
-		Price = price;
+		this.price = price;
+	}
+	
+	public Double getSubTotal() {
+		return price * quantity;
 	}
 
 	@Override
